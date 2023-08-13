a0b760fa15e0080f9a9ab14c805666966848e194
Piranja <techpiranja@gmail.com>
2023-08-13 23:48:31 +0200

    add favicons and mui icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import { MouseEvent, useEffect, useRef, useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CloseIcon from "@mui/icons-material/Close";
 import "./App.css";
 
 function App() {
@@ -18,30 +20,94 @@ function App() {
 		if (links.length > 0) localStorage.setItem("linkCollector-savedLinks", JSON.stringify(links));
 	}, [links]);
 
-	function addLink() {
-		const newLink = linkInput.current!.value;
-		if (newLink.trim() !== "") {
-			setLinks([...links, newLink]);
-			linkInput.current!.value = ""; // Clear input after saving
+	async function addLink() {
+		const newLink = linkInput.current!.value.trim();
+
+		if (newLink === "") {
+			return; // Do nothing if the input is empty
 		}
+
+		// Check if the link already exists in the links array
+		if (links.includes(newLink) || links.includes(`https://${newLink}`)) {
+			// Handle duplicate link case here, such as showing an error message
+			alert("Duplicate link. Please enter a unique link.");
+			linkInput.current!.value = "";
+			return;
+		}
+
+		// Add "https://" if missing and the link doesn't start with "http"
+		const formattedLink = !newLink.startsWith("http") ? `https://${newLink}` : newLink;
+
+		// Fetch favicon URL
+		try {
+			setLinks([...links, formattedLink]);
+			linkInput.current!.value = ""; // Clear input after saving
+		} catch (error) {
+			console.error("Error fetching link preview:", error);
+		}
+
+		setLinks([...links, formattedLink]);
+		linkInput.current!.value = ""; // Clear input after saving
+	}
+
+	function deleteAllLinks() {
+		setLinks([]);
+		localStorage.removeItem("linkCollector-savedLinks");
+	}
+
+	function getCleanLink(link: string) {
+		return link.replace(/^https?:\/\//i, ""); // Remove "http://" or "https://"
+	}
+
+	function getFaviconUrl(domain: string) {
+		return `https://www.google.com/s2/favicons?domain=${encodeURIComponent(domain)}&sz=${256}`;
+	}
+
+	function deleteItem(event: MouseEvent<HTMLButtonElement>, link: string): void {
+		event.preventDefault();
+		const newLinks = links.filter((item) => item !== link);
+		setLinks(newLinks);
+		localStorage.setItem("linkCollector-savedLinks", JSON.stringify(newLinks));
 	}
 
 	return (
 		<div className="wrapper">
 			<h1>Link Collector </h1>
 			<div className="linkCont">
-				<input type="text" placeholder="Your Link" ref={linkInput} />
+				<input
+					type="text"
+					placeholder="Your Link"
+					ref={linkInput}
+					onKeyDown={(event) => {
+						if (event.key === "Enter") addLink();
+					}}
+				/>
 				<button onClick={addLink}>Save</button>
 			</div>
 			<div className="linkWrapperCont">
-				{links.length > 0 && <h4 style={{ marginTop: 10 }}>Saved Links: </h4>}
-				<div className="linkWrapper">
-					{links.map((link, index) => (
-						<a key={index} href={link}>
-							{link}
-						</a>
-					))}
-				</div>
+				{links.length > 0 && (
+					<>
+						<div className="collectionHeader">
+							<h4 style={{ margin: 0 }}>Saved links: </h4>
+							<button onClick={deleteAllLinks} className="iconButton">
+								<DeleteIcon />
+								<p>Delete all links</p>
+							</button>
+						</div>
+
+						<div className="linkWrapper">
+							{links.map((link, index) => (
+								<a key={index} className="linkBox" href={link}>
+									<button onClick={(e) => deleteItem(e, link)} className="deleteItem">
+										<CloseIcon />
+									</button>
+									<img src={getFaviconUrl(link)} alt="Favicon" className="favicon" />
+									<p>{getCleanLink(link)}</p>
+								</a>
+							))}
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
